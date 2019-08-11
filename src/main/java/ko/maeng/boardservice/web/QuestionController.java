6407c53d92b4615abbbf4740618743aea3bbc57d
Rebwon <msolo021015@naver.com>
2019-08-11 14:53:48 +0900

    수정 삭제 기능 구현

--- src/main/java/ko/maeng/boardservice/web/QuestionController.java
+++ src/main/java/ko/maeng/boardservice/web/QuestionController.java
@@ -6,9 +6,8 @@ import ko.maeng.boardservice.domain.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
 
@@ -45,6 +44,61 @@ public class QuestionController {
 
         questionRepository.save(newQuestion);
 
+        return "redirect:/";
+    }
+
+    @GetMapping("/{id}")
+    public String show(@PathVariable Long id, Model model){
+        model.addAttribute("question", questionRepository.findById(id).get());
+        return "/qna/show";
+    }
+
+    @GetMapping("/{id}/form")
+    public String updateForm(@PathVariable Long id, Model model, HttpSession session){
+        if(!HttpSessionUtils.isLogin(session)){
+            log.info("Login Please!");
+            return "/users/loginForm";
+        }
+        User loginUser = (User) HttpSessionUtils.getUserFromSession(session);
+        Question question = questionRepository.findById(id).get();
+        if(!question.isSameWriter(loginUser)){
+            return "/users/loginForm";
+        }
+
+        model.addAttribute("question", question);
+        return "/qna/updateForm";
+    }
+
+    @PutMapping("/{id}")
+    public String update(@PathVariable Long id, String title, String contents, HttpSession session){
+        if(!HttpSessionUtils.isLogin(session)){
+            log.info("Login Please!");
+            return "/users/loginForm";
+        }
+        User loginUser = (User) HttpSessionUtils.getUserFromSession(session);
+        Question question = questionRepository.findById(id).get();
+        if(!question.isSameWriter(loginUser)){
+            return "/users/loginForm";
+        }
+
+        question.update(title, contents);
+        questionRepository.save(question);
+        return String.format("redirect:/questions/%d", id);
+    }
+
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable Long id, HttpSession session){
+        if(!HttpSessionUtils.isLogin(session)){
+            log.info("Login Please!");
+            return "/users/loginForm";
+        }
+        User loginUser = (User) HttpSessionUtils.getUserFromSession(session);
+        Question question = questionRepository.findById(id).get();
+        if(!question.isSameWriter(loginUser)){
+            return "/users/loginForm";
+        }
+
+        questionRepository.deleteById(id);
         return "redirect:/";
     }
 }
